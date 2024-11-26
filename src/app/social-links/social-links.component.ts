b567dffd84f33510692ba7aebb3f83e616acb16f
Tamas Niks <tamas.niks@gmail.com>
2024-11-26 13:17:24 +0100

    Versie Release post Heavy

--- src/app/social-links/social-links.component.ts
+++ src/app/social-links/social-links.component.ts
@@ -34,13 +34,13 @@ export class SocialLinksComponent implements OnInit {
         url = "https://www.tiktok.com/@betweentheratsandsnakes";
         break;
       case socialEnum.spotify:
-        url = "https://open.spotify.com/track/7gAvk3fVd5PwAMJcpGYlc5?si=4f8a764b47e94eab";
+        url = "https://open.spotify.com/artist/71mU3UyTHM9R5CYlbubxL0?si=iygWkmrqR6SWaS_rg2O3-w";
         break;
       case socialEnum.youtube:
         url = "https://www.youtube.com/channel/UCebRKhaXrssVdPNDSxWq0OQ/featured";
         break;
       case socialEnum.appleMusic:
-        url = "https://music.apple.com/us/album/the-prophet/1684824201?i=1684824202";
+        url = "https://music.apple.com/us/artist/between-the-rats-and-snakes/1650027932";
         break;
     }
     window.open(url, "_blank");
