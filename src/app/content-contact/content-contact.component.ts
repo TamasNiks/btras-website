ada49a20f8779ff840e01a75c74e8482da7728bb
Tamas Niks <tamas.niks@gmail.com>
2025-02-25 22:50:27 +0100

    Popronde aanpassing

--- src/app/content-contact/content-contact.component.ts
+++ src/app/content-contact/content-contact.component.ts
@@ -13,8 +13,8 @@ export class ContentContactComponent implements OnInit {
   public downloadPressKit(): void {
     let link = document.createElement("a");
     link.setAttribute("type", "hidden");
-    link.href = "assets/betweentheratsandsnakes_presskit_2024.zip";
-    link.download = "betweentheratsandsnakes_presskit_2024.zip";
+    link.href = "assets/betweentheratsandsnakes_presskit_2025_02_25.zip";
+    link.download = "betweentheratsandsnakes_presskit_2025_02_25.zip";
     document.body.appendChild(link);
     link.click();
     link.remove();
